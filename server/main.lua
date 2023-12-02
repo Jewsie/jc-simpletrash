0bd0f307038827b8e232c2a2dff014d498a80a0d
Jewsie <oliphil6@gmail.com>
2023-12-02 09:49:16 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,7 +1,25 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
+local trashData = {}
+local hasSearched = false
+
+RegisterServerEvent('jc-simpletrash:server:searchtrash')
+AddEventHandler('jc-simpletrash:server:searchtrash', function(data)
+    local src = source
+
+    if not trashData[data.entity] then
+        trashData[data.entity] = {hasSearched = false}
+    end
+
+    if not trashData[data.entity].hasSearched then
+        TriggerClientEvent('jc-simpletrash:client:searchtrash', src, data.entity)
+    else
+        QBCore.Functions.Notify(src, 'You have already searched this trashcan!', 'error', 3000)
+    end
+end)
+
 RegisterServerEvent('jc-simpletrash:server:giveitems')
-AddEventHandler('jc-simpletrash:server:giveitems', function()
+AddEventHandler('jc-simpletrash:server:giveitems', function(entity)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     local chance = math.random(1, 100)
@@ -28,4 +46,9 @@ AddEventHandler('jc-simpletrash:server:giveitems', function()
     else
         QBCore.Functions.Notify(src, 'You didn\'t find anything in this trashcan!', 'error', 3000)
     end
+
+    trashData[entity].hasSearched = true
+
+    Wait(1800 * 1000)
+    trashData[entity].hasSearched = false
 end)
