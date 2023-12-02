9c5328b039ba17e43c8504a8a812f61c46571eb0
Jewsie <oliphil6@gmail.com>
2023-12-02 10:32:09 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -7,15 +7,32 @@ Citizen.CreateThread(function()
                 label = 'Search trashcan',
                 icon = 'fas fa-trashcan',
                 targeticon = 'fas fa-eye',
-                type = 'server',
-                event = 'jc-simpletrash:server:searchtrash'
+                action = function()
+                    local playerPos = GetEntityCoords(PlayerPedId())
+                    local closestTrashcan = 0
+
+                    for _, binModel in pairs(Config.Trashcans) do
+                        closestTrashcan = GetClosestObjectOfType(playerPos.x, playerPos.y, playerPos.z, 1.0, GetHashKey(binModel), false, false, false)
+                        
+                        if closestTrashcan ~= 0 then
+                            break
+                        end
+                    end
+
+                    if closestTrashcan ~= 0 then
+                        local binPos = GetEntityCoords(closestTrashcan)
+                        TriggerServerEvent('jc-simpletrash:server:searchtrash', binPos)
+                    else
+                        print("No trashcan found nearby.")
+                    end
+                end
             }
         }
     })
 end)
 
 RegisterNetEvent('jc-simpletrash:client:searchtrash')
-AddEventHandler('jc-simpletrash:client:searchtrash', function(entity)
+AddEventHandler('jc-simpletrash:client:searchtrash', function(binPosition)
     TaskStartScenarioInPlace(PlayerPedId(), "PROP_HUMAN_PARKING_METER", 10000, false)
     QBCore.Functions.Progressbar('searching_trash', 'Searching Trashcan...', 10000, false, true, {
         disableMovement = true,
@@ -23,7 +40,7 @@ AddEventHandler('jc-simpletrash:client:searchtrash', function(entity)
         disableMouse = false,
         disableCombat = true
         }, {}, {}, {}, function()
-            TriggerServerEvent('jc-simpletrash:server:giveitems', entity)
+            TriggerServerEvent('jc-simpletrash:server:giveitems', binPosition)
             ClearPedTasks(PlayerPedId())
         end, function()
             ClearPedTasks(PlayerPedId())
